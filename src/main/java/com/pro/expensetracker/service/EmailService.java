829fbd43a1eaccdd15c78d5c1a8fd0e0bf317f28
ManjunathD4 <desaimanju2001@gmail.com>
2026-02-24 13:13:22 +0530

    brevo with api

--- src/main/java/com/pro/expensetracker/service/EmailService.java
+++ src/main/java/com/pro/expensetracker/service/EmailService.java
@@ -1,11 +1,18 @@
 package com.pro.expensetracker.service;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.management.RuntimeErrorException;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,58 +20,61 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class EmailService {
 
-	private final JavaMailSender mailsender;
-	
-	@Value("${spring.mail.properties.mail.smtp.from}")
-	private String fromEmail;
-	
-	public void sendEmail(String to,String subject,String body) {
-		try {
-			SimpleMailMessage message = new SimpleMailMessage();
-			message.setFrom(fromEmail);
-			message.setTo(to);
-			message.setSubject(subject);
-			message.setText(body);
-			mailsender.send(message);
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-	}
 	/*
-	 * @Value("${BREVO_API_KEY}") private String apiKey;
+	 * private final JavaMailSender mailsender;
 	 * 
-	 * @Value("${brevo.sender.email}") private String fromEmail;
+	 * @Value("${brevo.api.key}") private String brevoApiKey;
 	 * 
-	 * @Value("${brevo.sender.name}") private String senderName;
+	 * @Value("${brevo.sender.email}") private String senderEmail;
 	 * 
-	 * private final RestTemplate restTemplate = new RestTemplate();
+	 * @Value("${spring.mail.properties.mail.smtp.from}") private String fromEmail;
 	 * 
-	 * public void sendEmail(String to, String subject, String body) { try { String
-	 * url = "https://api.brevo.com/v3/smtp/email";
+	 * public void sendEmail(String to,String subject,String body) { try {
 	 * 
-	 * HttpHeaders headers = new HttpHeaders(); headers.add("accept",
-	 * "application/json"); headers.add("content-type", "application/json");
-	 * headers.add("api-key", apiKey);
+	 * SimpleMailMessage message = new SimpleMailMessage();
+	 * message.setFrom(fromEmail); message.setTo(to); message.setSubject(subject);
+	 * message.setText(body); mailsender.send(message);
 	 * 
-	 * Map<String, Object> requestBody = Map.of( "sender", Map.of( "name",
-	 * senderName, "email", fromEmail ), "to", List.of( Map.of("email", to) ),
-	 * "subject", subject, "htmlContent", body );
 	 * 
-	 * HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody,
-	 * headers);
-	 * 
-	 * System.out.println("➡ Calling Brevo API...");
-	 * System.out.println("➡ API KEY PRESENT: " + (apiKey != null));
-	 * 
-	 * ResponseEntity<String> response = restTemplate.postForEntity(url, request,
-	 * String.class);
-	 * 
-	 * System.out.println("✅ Brevo Response Status: " + response.getStatusCode());
-	 * System.out.println("✅ Brevo Response Body: " + response.getBody());
-	 * 
-	 * } catch (Exception e) { e.printStackTrace(); throw new
-	 * RuntimeException("Failed to send email: " + e.getMessage()); } }
+	 * } catch (Exception e) { throw new RuntimeException(e.getMessage()); } }
 	 */
+
 	
-		
+	  @Value("${BREVO_API_KEY}") 
+	  private String apiKey;
+	  
+	  @Value("${BREVO_FROM_MAIL}")
+	  private String fromEmail;
+	  
+	  @Value("${brevo.sender.name}")
+	  private String senderName;
+	  
+	  private final RestTemplate restTemplate = new RestTemplate();
+	  
+	  public void sendEmail(String to, String subject, String body) { try { String
+	  url = "https://api.brevo.com/v3/smtp/email";
+	  
+	  HttpHeaders headers = new HttpHeaders(); 
+	  headers.add("accept",
+	  "application/json");
+	  headers.add("content-type", "application/json");
+	  headers.add("api-key", apiKey);
+	  
+	  Map<String, Object> requestBody = Map.of( "sender", Map.of( "name",
+	  senderName, "email", fromEmail ), "to", List.of( Map.of("email", to) ),
+	  "subject", subject, "htmlContent", body );
+	  
+	  HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody,
+	  headers);
+	  
+	
+	  ResponseEntity<String> response = restTemplate.postForEntity(url, request,
+	  String.class);
+	  
+	 
+	  
+	  } catch (Exception e) { e.printStackTrace(); throw new
+	  RuntimeException("Failed to send email: " + e.getMessage()); } }
+	 
+
 }
